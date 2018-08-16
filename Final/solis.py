c0b171bce516e65de2f29abf7d5449184b834016
Chris <chris@brogard.io>
2018-08-16 05:44:40 -0500

    Fix bug in "Data.Wrangling"

--- Final/solis.py
+++ Final/solis.py
@@ -12,3 +12,10 @@ def print_file_from_head(fname, nlines):
         with open(fname) as f:
                 for line in islice(f, nlines):
                         print("".join(line.rstrip()))
+
+def load_csv(fname):
+    import pandas as pd
+    clean_dir = "data/clean"
+    csv_file = "{}/{}".format(clean_dir, fname)
+    result = pd.read_csv(csv_file)
+    return result
